59e1663e264367b1d1f7fb15fc256a547c9e1351
Lu Ye Zhan <lu.ye.external@axa.com>
2020-02-19 10:22:33 +0100

    using services for cross components comunication

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -13,7 +13,11 @@ export class NewAccountComponent {
   constructor(
     private loggingService: LoggingService,
     private accountService: AccountsService
-  ) {}
+  ) {
+    this.accountService.statusUpdated.subscribe((status: string) =>
+      alert("New Status:" + status)
+    );
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountService.addAccount(accountName, accountStatus);
